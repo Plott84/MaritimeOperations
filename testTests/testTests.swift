24fde48c3dc337d0756221d9ee4d986fe015d32a
Henning Olsen <post@henningolsen.no>
2026-04-07 06:48:59 +0800

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Henning Andre Olsen on 07/04/2026.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
